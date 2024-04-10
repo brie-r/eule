9c5b4b4be712e46813ef7d41d6790f2924f57277
brie-r <57336070+brie-r@users.noreply.github.com>
2024-04-09 23:17:56 -0500

    json -> ron, less code duplication

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ use serde::{Serialize, Deserialize};
 use std::path::PathBuf;
 use directories::ProjectDirs;
 use anyhow::{Result, Context};
-use tokio::{fs, fs::OpenOptions, io::{self, AsyncReadExt, AsyncWriteExt}, sync::Mutex};
+use tokio::{fs, fs::OpenOptions, io::{AsyncReadExt, AsyncWriteExt}, sync::Mutex};
 use std::sync::Arc;
-
 pub struct Serder
 {
 	path: PathBuf,
@@ -18,38 +17,28 @@ impl Serder
 		fs::create_dir_all(data_dir).await?;
 		Ok(Serder { path: data_dir.into() })
 	}
-	pub async fn deserialize_or_default<T>(&self, filename: String) -> io::Result<T>
+	pub async fn deserialize_or_default<T>(&self, filename: String) -> anyhow::Result<T>
 	where
 		T: Default + for<'de> Deserialize<'de>
 	{
-		let filepath = self.path.join(filename);
-		let mut file = OpenOptions::new().read(true).write(true).create(true).open(filepath).await?;
-
-		let mut buf = Vec::new();	
-		let size = file.read_to_end(&mut buf).await?;
-
-		let output: T = match size
+		let buf = self.read_file(filename).await?;
+		let output = match buf.len()
 		{
 			0 => Default::default(),
-			1.. => serde_json::from_slice(&buf)?,
+			1.. => ron::de::from_bytes(&buf).unwrap_or_default(),
 		};
 
 		Ok(output)
 	}
-		pub async fn deserialize_or_value<T>(&self, filename: String, value: T) -> io::Result<T>
+	pub async fn deserialize_or_value<T>(&self, filename: String, value: T) -> anyhow::Result<T>
 	where
 		T: for<'de> Deserialize<'de>
 	{
-		let filepath = self.path.join(filename);
-		let mut file = OpenOptions::new().read(true).write(true).create(true).open(filepath).await?;
-
-		let mut buf = Vec::new();	
-		let size = file.read_to_end(&mut buf).await?;
-
-		let output: T = match size
+		let buf = self.read_file(filename).await?;
+		let output = match buf.len()
 		{
 			0 => value,
-			1.. => serde_json::from_slice(&buf)?,
+			1.. => ron::de::from_bytes(&buf).unwrap_or(value),
 		};
 
 		Ok(output)
@@ -59,42 +48,41 @@ impl Serder
 		T: for<'de> Deserialize<'de>
 	{
 		let buf = self.read_file(filename).await?;
-	
 		let output = match buf.len()
 		{
 			0 => None,
-			1.. => Some(serde_json::from_slice(&buf)?),
+			1.. => Some(ron::de::from_bytes(&buf)?),
 		}.context("File empty.")?;
 		Ok(output)
 	}
-	pub async fn serialize_and_save<T>(&self, filename: String, data: T) -> io::Result<()>
+	pub async fn serialize_and_save<T>(&self, filename: String, data: T) -> anyhow::Result<()>
 	where
 		T: Serialize
 	{
 		let filepath = self.path.join(filename);
 		let mut file = OpenOptions::new().truncate(true).write(true).create(true).open(filepath).await?;
-		let serialized_data = serde_json::to_string(&data)?;
+		let serialized_data = ron::ser::to_string(&data)?;
 		file.write_all(serialized_data.as_bytes()).await?;
 		Ok(())
 	}
-	pub async fn serialize_arc_and_save<T>(&self, filename: String, data: Arc<T>) -> io::Result<()>
+	pub async fn serialize_arc_and_save<T>(&self, filename: String, data: Arc<T>) -> anyhow::Result<()>
 	where
 		T: Serialize
 	{
 		let filepath = self.path.join(filename);
 		let mut file = OpenOptions::new().truncate(true).write(true).create(true).open(filepath).await?;
-		let serialized_data = serde_json::to_string(data.as_ref())?;
+		let serialized_data = ron::ser::to_string(data.as_ref())?;
 		file.write_all(serialized_data.as_bytes()).await?;
 		Ok(())
 	}
-	pub async fn serialize_arc_mutex_and_save<T>(&self, filename: String, data: Arc<Mutex<T>>) -> io::Result<()>
+	pub async fn serialize_arc_mutex_and_save<T>(&self, filename: String, data: Arc<Mutex<T>>) -> anyhow::Result<()>
 	where
 		T: Serialize
 	{
 		let filepath = self.path.join(filename);
 		let mut file = OpenOptions::new().truncate(true).write(true).create(true).open(filepath).await?;
 		let locked_data = data.lock().await;
-		let serialized_data = serde_json::to_string(&*locked_data)?;
+		let serialized_data = ron::ser::to_string(&*locked_data)?;
 		file.write_all(serialized_data.as_bytes()).await?;
 		Ok(())
 	}
@@ -143,26 +131,35 @@ mod tests
 		CA, CB, CC
 	}
 	#[tokio::test]
-	async fn default_or_value() -> Result<()>
+	async fn deserialize_or_value() -> Result<()>
 	{
 		let se = Serder::new("eule_test".to_string()).await?;
 		let mut hm: HashMap<String, E<EA, EB, EC>> = HashMap::new();
 		hm.insert("B".to_string(), E::B{v: Some(EB::BC)});
-		let data: HashMap<String, E<EA, EB, EC>> = se.deserialize_or_value("in_no_err.json".to_string(), hm).await?;
-		se.serialize_and_save("out_no_err.json".to_string(), data).await?;
+		let data: HashMap<String, E<EA, EB, EC>> = se.deserialize_or_value("in_no_err.ron".to_string(), hm).await?;
+		se.serialize_and_save("out_no_err.ron".to_string(), data).await?;
 
 		Ok(())
 	}
 	#[tokio::test]
-	async fn default_or_value_arc_mutex() -> Result<()>
+	async fn deserialize_or_value_arc_mutex() -> Result<()>
 	{
 		let se = Serder::new("eule_test".to_string()).await?;
 		let mut hm: HashMap<String, E<EA, EB, EC>> = HashMap::new();
 		hm.insert("B".to_string(), E::B{v: Some(EB::BC)});
-		let data: HashMap<String, E<EA, EB, EC>> = se.deserialize_or_value("in_arc_mutex.json".to_string(), hm).await?;
+		let data: HashMap<String, E<EA, EB, EC>> = se.deserialize_or_value("in_arc_mutex.ron".to_string(), hm).await?;
 		let data_arc_mutex = Arc::new(Mutex::new(&data));
-		se.serialize_arc_mutex_and_save("out_arc_mutex.json".to_string(), Arc::clone(&data_arc_mutex)).await?;
-
+		se.serialize_arc_mutex_and_save("out_arc_mutex.ron".to_string(), Arc::clone(&data_arc_mutex)).await?;
+		Ok(())
+	}
+	#[tokio::test]
+	async fn serialize_arc_mutex_and_save() -> Result<()>
+	{
+		let se = Serder::new("eule_test".to_string()).await?;
+		let mut hm: HashMap<String, E<EA, EB, EC>> = HashMap::new();
+		hm.insert("B".to_string(), E::B{v: Some(EB::BC)});
+		let data_arc_mutex = Arc::new(Mutex::new(&hm));
+		se.serialize_arc_mutex_and_save("out_arc_mutex_save.ron".to_string(), Arc::clone(&data_arc_mutex)).await?;
 		Ok(())
 	}
 }
